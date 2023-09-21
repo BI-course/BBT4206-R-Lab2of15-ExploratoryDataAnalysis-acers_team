4b2ecbde762541d1f36a3d119ef053cd034a4cee
NickBwalley <nickbwalley@gmail.com>
2023-09-21 17:02:37 +0300

    aprecommit

---
+++ Lab2-Submission-ExploratoryDataAnalysis.R
@@ -0,0 +1,6 @@
+require("readr")
+
+library(readr)
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset <- read_csv("data/20230412-20230719-BI1-BBIT4-1-StudentPerformanceDataset.csv")
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
+
